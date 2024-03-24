16489506c184da0fb7764de167640f1ea56f322e
sarahafaran <74952683+sarahafaran@users.noreply.github.com>
2024-03-24 21:07:04 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-15b5235b-37d3-46b9-80bf-f85b063ccc0c/)
 
 
 echo "$curl_output" >> Update.tsx
